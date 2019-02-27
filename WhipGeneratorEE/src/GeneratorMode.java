75efb3f626e0b665fd5ba9232e164d7f87acea40
Hunter <hunterufford@gmail.com>
2019-02-27 13:12:16 -0800

    Test2

--- WhipGeneratorEE/src/GeneratorMode.java
+++ WhipGeneratorEE/src/GeneratorMode.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package WhipGeneratorEE.src;
 
 
@@ -15,19 +15,3 @@ public class GeneratorMode {
 	}
 	
 }
-
-=======
-import java.util.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-public class GeneratorMode {
-
-	public GeneratorMode()
-	{
-	
-	}
-	
-}
->>>>>>> 5197454b72a06628b899c816392ef81439e6a467
