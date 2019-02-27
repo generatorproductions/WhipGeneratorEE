5197454b72a06628b899c816392ef81439e6a467
Frogs65 <47994055+Frogs65@users.noreply.github.com>
2019-02-27 13:06:56 -0800

    Update GeneratorMode.java

--- WhipGeneratorEE/src/GeneratorMode.java
+++ WhipGeneratorEE/src/GeneratorMode.java
@@ -1,4 +1,8 @@
-
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class GeneratorMode {
 
 	public GeneratorMode()
